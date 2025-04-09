e714ae65700cba115ea812a5738e4d41b3c605f3
Thangmn2104 <minhthang.masterbranch@gmail.com>
2025-04-09 12:29:07 +0700

    fix min of order item equal 0

--- models/order.model.js
+++ models/order.model.js
@@ -1,70 +1,73 @@
-const mongoose = require('mongoose');
-const moment = require('moment-timezone');
+const mongoose = require("mongoose");
+const moment = require("moment-timezone");
 
 const orderItemSchema = new mongoose.Schema({
-    productId: {
-        type: String,
-        required: true
-    },
-    productName: {
-        type: String,
-        required: true
-    },
-    quantity: {
-        type: Number,
-        required: true,
-        min: 1
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: 0
-    }
+  productId: {
+    type: String,
+    required: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
 });
 
-const orderSchema = new mongoose.Schema({
+const orderSchema = new mongoose.Schema(
+  {
     userId: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     orderId: {
-        type: String,
-        unique: true
+      type: String,
+      unique: true,
     },
     customerName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     phone: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     total: {
-        type: Number,
-        required: true,
-        min: 0
+      type: Number,
+      required: true,
+      min: 0,
     },
     status: {
-        type: String,
-        enum: ['pending', 'completed', 'cancelled'],
-        default: 'pending'
+      type: String,
+      enum: ["pending", "completed", "cancelled"],
+      default: "pending",
     },
     completedDate: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     cancelledDate: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     note: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
-    orderItems: [orderItemSchema]
-}, {
-    timestamps: true
-});
+    orderItems: [orderItemSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Order = mongoose.model('Order', orderSchema);
-module.exports = Order; 
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
